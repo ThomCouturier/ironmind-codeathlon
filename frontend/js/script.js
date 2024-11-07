c2800e6c8ff6f9f675cc0aa15bd93d9a12829758
Keven Champagne <keven.champagne.2@ulaval.ca>
2024-11-06 23:34:43 -0500

    Final

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,8 +6,44 @@ $(document).ready(function () {
     const api = '/api/';
 
     $.ajax({
-        url: api + 'items',
+        url: api + 'account',
+        type: 'POST',
+        success: function (data) {
+            data = ["option1", "option2"]
+            var list = document.getElementById("programs");
+            data.forEach(element => {
+                var opt = document.createElement('option');
+                opt.value = element;
+                opt.innerHTML = element;
+                list.appendChild(opt);
+            });
+        },
+    });
+    $.ajax({
+        url: api + 'modifyAccount',
+        type: 'POST',
+        success: function (data) {
+            console.log(data);
+        },
+    });
+    $.ajax({
+        url: api + 'events',
         type: 'GET',
+        success: function (data) {
+            data = "test"
+            document.getElementById("events").innerHTML = data
+        },
+    });
+    $.ajax({
+        url: api + 'tickets',
+        type: 'GET',
+        success: function (data) {
+            console.log(data);
+        },
+    });
+    $.ajax({
+        url: api + 'connexion',
+        type: 'POST',
         success: function (data) {
             console.log(data);
         },
